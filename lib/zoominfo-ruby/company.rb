3a5dad46e669fb19632a27132c3534ac7624a215
Ian Morgan <ian@ruby-code.com>
2014-06-05 11:25:36 -0700

    add method for details by domain name

--- lib/zoominfo-ruby/company.rb
+++ lib/zoominfo-ruby/company.rb
@@ -2,7 +2,7 @@ module ZoomInfo
   class Company < ZoomInfo::Base
     def search(query = {})
       query = prepare_request(query)
-      self.class.get("/company/search", query: query).parsed_response['CompanySearchRequest']['CompanySearchResults']
+      self.class.get("/company/search", query: query).parsed_response
     end
 
     def detail(query = {})
@@ -11,7 +11,12 @@ module ZoomInfo
     end
 
     def search_by_company_name(company_name)
-      search('CompanyName' => company_name)
+      search('CompanyName' => company_name)['CompanySearchRequest']['CompanySearchResults']
     end
+
+    def detail_by_domain_name(domain_name)
+      detail('CompanyDomain' => domain_name)
+    end
+
   end
 end
